b452044ee7425654bcd7a4bc41210006825a82f4
gudals <gudals9677@gmail.com>
2024-03-04 16:43:37 +0900

    12

---
+++ 1장 데이터베이스와 DBMS 개요.sql
@@ -0,0 +1,38 @@
+/*
+    날짜 : 2024/03/04
+    이름 : 김형민
+    내용 : 1장 데이터베이스와 DBMS 개요
+*/
+
+
+-- 실습하기 1-2. NUMBER 자료형 이해
+CREATE TABLE TYPE_TEST_NUMBER (
+num1 NUMBER,
+num2 NUMBER(2),
+num3 NUMBER(3,1),
+num4 NUMBER(4,2),
+num5 NUMBER(5,6)
+);
+
+//num1 number
+insert into TYPE_TEST_NUMBER (num1) values (1);
+insert into TYPE_TEST_NUMBER (num1) values (123);
+insert into TYPE_TEST_NUMBER (num1) values (123.74);
+insert into TYPE_TEST_NUMBER (num1) values (123.12345);
+
+//num2 NUMBER(2)
+insert into TYPE_TEST_NUMBER (num2) values (12);
+insert into TYPE_TEST_NUMBER (num2) values (123);
+insert into TYPE_TEST_NUMBER (num2) values (1.2);
+insert into TYPE_TEST_NUMBER (num2) values (1.23);
+insert into TYPE_TEST_NUMBER (num2) values (12.34567);
+insert into TYPE_TEST_NUMBER (num2) values (12.56789);
+insert into TYPE_TEST_NUMBER (num2) values (123.56789);
+
+//num3 NUMBER(3,1)
+insert into TYPE_TEST_NUMBER (num3) values (12);
+insert into TYPE_TEST_NUMBER (num3) values (123);
+insert into TYPE_TEST_NUMBER (num3) values (12.1);
+insert into TYPE_TEST_NUMBER (num3) values (12.1234);
+insert into TYPE_TEST_NUMBER (num3) values (12.56789);
+insert into TYPE_TEST_NUMBER (num3) values (123.56789);
